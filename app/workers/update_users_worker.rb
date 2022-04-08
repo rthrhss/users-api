2aacc7d0a3af1f31278745a405a3552c4b11016a
Arthur Hess <rthrhss@users.noreply.github.com>
2022-04-08 01:54:26 -0300

    feat: add UpdateUsersWorker

---
+++ app/workers/update_users_worker.rb
@@ -0,0 +1,19 @@
+class UpdateUsersWorker
+  include Sidekiq::Worker
+
+  def perform
+    response = GetUsersData.call(api_key: api_key)
+
+    raise StandardError, response.message if response.failure?
+
+    response.users_data.each do |user_attributes|
+      UpsertUser.call(user_attributes)
+    end
+  end
+
+  private
+
+  def api_key
+    ENV['USERS_API_KEY']
+  end
+end
---
+++ spec/workers/update_users_worker_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe UpdateUsersWorker, type: :worker do
+  # What am I really testing here?
+  # I could test that the worker is enqueueing jobs correctly, but that's
+  # probably not the point.
+  #
+  context 'when request is successful' do
+    it 'should create or update users' do
+      api_response = [{ id: 1, first_name: 'John', last_name: 'Doe' }]
+
+      allow(GetUsersData)
+        .to receive(:call)
+        .and_return(double(failure?: false, users_data: api_response))
+
+      expect { described_class.new.perform }.to change { User.count }.by(1)
+    end
+  end
+
+  context 'when request is not successful' do
+    it 'should raise an error' do
+      allow(GetUsersData)
+        .to receive(:call)
+        .and_return(double(failure?: true, message: 'API error'))
+
+      expect { described_class.new.perform }.to raise_error(StandardError, 'API error')
+    end
+  end
+end
